61618e32cd0322a61207e5d53b5b230d285ead97
Ed Carter <bittenbytailfly@server.fake>
2018-07-26 12:51:55 +0100

    Functional but needs tidying.

--- TenTwentyFour.Wingman.UserInterface/App_Start/RouteConfig.cs
+++ TenTwentyFour.Wingman.UserInterface/App_Start/RouteConfig.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using TenTwentyFour.Wingman.UserInterface.RouteConstraints;
 
 namespace TenTwentyFour.Wingman.UserInterface
 {
@@ -13,11 +15,55 @@ namespace TenTwentyFour.Wingman.UserInterface
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            var maxDimensionInt = 500;
+            var maxDimension = ConfigurationManager.AppSettings["MaximumImageDimension"];
+            if (maxDimension == null)
+            {
+                throw new ConfigurationErrorsException("You must specify a key for MaximumImageDimension");
+            }
+
+            if (!Int32.TryParse(maxDimension, out maxDimensionInt))
+            {
+                throw new ConfigurationErrorsException("You must specify a valid integer for MaximumImageDimension");
+            }
+
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            var maxImageSizeConstraint = new MaximumImageSizeConstraint(maxDimensionInt);
+
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                name: "Crop Square With Format Change",
+                url: "derived/square/{quality}/{originalExtension}/{size}/{*path}",
+                defaults: new { controller = "ImageServe", action = "Square" },
+                constraints: new { size = maxImageSizeConstraint, path = "(.*).(jpg|png|gif|webp)", originalExtension = "jpg|png|gif|webp" }
             );
+
+            routes.MapRoute(
+               name: "Crop Square",
+               url: "derived/square/{quality}/{size}/{*path}",
+               defaults: new { controller = "ImageServe", action = "Square" },
+               constraints: new { size = maxImageSizeConstraint, path = "(.*).(jpg|png|gif|webp)" }
+           );
+
+            routes.MapRoute(
+                name: "Resize Width With Format Change",
+                url: "derived/resize-w/{quality}/{originalExtension}/{size}/{*path}",
+                defaults: new { controller = "ImageServe", action = "ResizeToWidth" },
+                constraints: new { size = maxImageSizeConstraint, path = "(.*).(jpg|png|gif|webp)", originalExtension = "jpg|png|gif|webp" }
+            );
+
+            routes.MapRoute(
+                name: "Resize Width",
+                url: "derived/resize-w/{quality}/{size}/{*path}",
+                defaults: new { controller = "ImageServe", action = "ResizeToWidth" },
+                constraints: new { size = maxImageSizeConstraint, path = "(.*).(jpg|png|gif|webp)" }
+            );
+
+            routes.MapRoute(
+               name: "Plain",
+               url: "{*path}",
+               defaults: new { controller = "ImageServe", action = "Plain" }
+           );
         }
     }
 }
---
+++ TenTwentyFour.Wingman.UserInterface/Controllers/ImageServeController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TenTwentyFour.Wingman.ImageManipulator;
+using TenTwentyFour.Wingman.ImageManipulator.Manipulations;
+
+namespace TenTwentyFour.Wingman.UserInterface.Controllers
+{
+    public class ImageServeController : Controller
+    {
+        ManipulationService Service { get; set; }
+
+        public ImageServeController(ManipulationService service)
+        {
+            this.Service = service;
+        }
+
+        public ImageServeController()
+            : this(new ManipulationService(ConfigurationManager.AppSettings["SourceDirectory"], ConfigurationManager.AppSettings["DerivedDirectory"]))
+            { }
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+            base.HttpContext.Response.AppendHeader("Cache-Control", "max-age=2592000");
+        }
+
+        public ActionResult Plain(string path)
+        {
+            var mimeType = this.Service.GetMimeType(Path.GetExtension(path));
+            var originPath = Path.Combine(this.Service.SourceDirectory, path);
+
+            if (System.IO.File.Exists(originPath))
+            {
+                base.HttpContext.Response.AppendHeader("Cache-Control", "max-age=2592000");
+                return base.File(originPath, mimeType);
+            }
+
+            throw new HttpException(404, "File not found");
+        }
+
+        public ActionResult Square(int quality, int size, string path, string originalExtension = null)
+        {
+            var fileExtension = Path.GetExtension(path);
+            var derivedFileName = "square_" + size + fileExtension;
+            var manipulation = new SquareManipulation(size, quality);
+
+            return this.ServeManipulatedImage(path, derivedFileName, originalExtension, manipulation);
+        }
+
+        public ActionResult ResizeToWidth(int quality, int width, string path, string originalExtension = null)
+        {
+            var fileExtension = Path.GetExtension(path);
+            var derivedFileName = "resize_w_" + width + fileExtension;
+            var manipulation = new ResizeToWidthManipulation(width, quality);
+
+            return this.ServeManipulatedImage(path, derivedFileName, originalExtension, manipulation);
+        }
+
+        #region Helper Methods
+
+        public ActionResult ServeManipulatedImage(string relativePath, string derivedFileName, string originalExtension, Manipulation imageManipulation)
+        {
+            try
+            {
+                var derivedImage = this.Service.DeriveManipulatedImage(relativePath, derivedFileName, originalExtension, imageManipulation);
+                return base.File(derivedImage.FilePath, derivedImage.MimeType);
+            }
+            catch (FileNotFoundException ex)
+            {
+                throw new HttpException(404, "File not found");
+            }
+            catch (Exception ex)
+            {
+                throw new HttpException(500, ex.Message, ex);
+            }
+        }
+
+        #endregion
+    }
+}
